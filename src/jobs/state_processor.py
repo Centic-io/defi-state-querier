b9ba4fc104cc074c3a61ab4655a0a93fd28849cb
phamvietbang <superpower99x99@gmail.com>
2023-08-25 10:26:08 +0700

    fix get prices token

--- src/jobs/state_processor.py
+++ src/jobs/state_processor.py
@@ -10,7 +10,7 @@ from utils.init_services import init_services
 
 
 class StateProcessor:
-    def __init__(self, provider_uri: str, chain_id: str, mongodb: MongoDB):
+    def __init__(self, provider_uri: str, chain_id: str, mongodb: MongoDB = None):
         self.mongodb = mongodb
         self.state_querier = StateQuerier(provider_uri)
         self.chain_id = chain_id
@@ -92,7 +92,7 @@ class StateProcessor:
 
         return result
 
-    def run(self, wallet: str, queries: list, block_number: int = 'latest'):
+    def run(self, wallet: str, queries: list, block_number: int = 'latest', batch_size: int = 100, max_workers: int = 8):
         all_rpc_calls, all_tokens = {}, []
         for query in queries:
             query_id = query.get("query_id")
@@ -103,8 +103,10 @@ class StateProcessor:
             all_rpc_calls.update(rpc_calls)
             all_tokens += tokens
         result = []
-        decoded_data = self.execute_rpc_calls(all_rpc_calls)
-        token_prices = self.get_token_prices(all_tokens)
+        decoded_data = self.execute_rpc_calls(all_rpc_calls, batch_size, max_workers)
+        token_prices = {}
+        if self.mongodb:
+            token_prices = self.get_token_prices(all_tokens)
         for query in queries:
             query_id = query.get("query_id")
             query_type = query.get("query_type")
