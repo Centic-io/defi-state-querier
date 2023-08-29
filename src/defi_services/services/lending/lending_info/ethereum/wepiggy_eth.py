3fad1ca378d2df03c09780ff7ec4ace010b16d06
ngocban284 <daongocban284@gmail.com>
2023-08-29 17:34:43 +0700

    feat : wepiggy lending info

---
+++ src/defi_services/services/lending/lending_info/ethereum/wepiggy_eth.py
@@ -0,0 +1,41 @@
+COMPOUND_ETH = {
+    "name": "Wepiggy Lending Pool",
+    "rewardToken": "0x6f620ec89b8479e97a6985792d0c64f237566746",
+    "comptrollerAddress": "0x0c8c1ab017c3c0c8a48dd9f1db2f59022d190f0b",
+    "lensAddress": "0x6a05bd123d780055c38526cc05d3c9b90d0e471c",
+    "distributionAddress": "0x3e5496e50793e72e6143a15bed1c2535f0b0b9b0",
+    "poolToken": "0x6f620ec89b8479e97a6985792d0c64f237566746",
+    "type": "LENDING_POOL",
+    "forked": "compound",
+    "reservesList": {
+        "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984": {
+            "cToken": "0x82413f75f0da101e0fe7f6ff6cba3461f7e04f29",
+            "liquidationThreshold": 0.6
+        },
+        "0x6b175474e89094c44da98b954eedeac495271d0f": {
+            "cToken": "0x85166b72c87697a6acff24101b43fd54fe28a179",
+            "liquidationThreshold": 0.75
+        },
+        "0xdac17f958d2ee523a2206206994597c13d831ec7": {
+            "cToken": "0x5cfad792c4df1323188180778aec58e00eace32a",
+            "liquidationThreshold": 0.8
+        },
+        "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48": {
+            "cToken": "0xf8e5b9738bf63adfff36a849f9b9c9617c8d8c1f",
+            "liquidationThreshold": 0.8
+        },
+        "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599":{
+            "cToken": "0xc12b9d620bfcb48be3e0ccbf0ea80c717333b46f",
+            "liquidationThreshold": 0.7
+        },
+        "0x0000000000000000000000000000000000000000": {
+            "cToken": "0x27a94869341838d5783368a8503fda5fbcd7987c",
+            "liquidationThreshold": 0.8
+        },
+        "0xbbbbca6a901c926f240b89eacb641d8aec7aeafd": {
+            "cToken": "0x690aa2591e57180cba5a6123e9d462907a5e1c95",
+            "liquidationThreshold": 0.5
+        },
+
+    }
+}
