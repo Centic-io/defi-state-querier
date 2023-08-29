1f793e2aaa91886b249c298873220c72380f6d4c
Thang Nguyen <thangnguyen@Thangs-MacBook-Pro.local>
2023-08-29 17:54:42 +0700

    feat: liqee lending protocol

--- src/defi_services/constants/entities/lending.py
+++ src/defi_services/constants/entities/lending.py
@@ -8,6 +8,7 @@ from defi_services.services.lending.radiant_v2_services import RadiantStateServi
 from defi_services.services.lending.trava_services import TravaStateService
 from defi_services.services.lending.valas_services import ValasStateService
 from defi_services.services.lending.venus_services import VenusStateService
+from defi_services.services.lending.liqee_service import LiqeeStateService
 
 
 class Lending:
@@ -21,7 +22,8 @@ class Lending:
     geist = "geist-finance"
     cream = "cream-lending"
     venus = "venus"
-    all = [aave_v2]
+    liqee = "liqee"
+    all = [liqee]
 
     # chain
     ethereum = {
@@ -29,6 +31,7 @@ class Lending:
         compound: CompoundStateService,
         trava: TravaStateService,
         aave_v3: AaveV3StateService,
+        liqee: LiqeeStateService
     }
     fantom = {
         trava: TravaStateService,
