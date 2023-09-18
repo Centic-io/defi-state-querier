c8606486a0d4492020dba77c0665c06af5591d35
ngocban284 <daongocban284@gmail.com>
2023-09-18 15:06:35 +0700

    feat : justlend lending info

---
+++ src/defi_services/services/lending/lending_info/tron/justlend_tron.py
@@ -0,0 +1,87 @@
+JUST_LEND_TRON = {
+    "name": "JustLend Lending Pool",
+    "rewardToken": "TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn",
+    "comptrollerAddress": "TB23wYojvAsSx6gR8ebHiBqwSeABiBMPAr",
+    "lensAddress": "TXTXGyhNLhELNZPDXsn5fCnGYLZoLwJvRC",
+    "poolToken": "TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn",
+    "type": "LENDING_POOL",
+    "forked": "compound",
+    "reservesList": {
+        "0": {
+            "cToken": "TE2RzoSV3wFK99w6J9UnnZ4vLfXYoxvRwP",
+            "liquidationThreshold": 0.75
+        },
+        "TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn": {
+            "cToken": "TX7kybeP6UwTBRHLNPYmswFESHfyjm9bAS",
+            "liquidationThreshold": 0.85
+        },
+        "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t": {
+            "cToken": "TXJgMdjVX5dKiQaUi9QobwNxtSQaFqccvd",
+            "liquidationThreshold": 0.75
+        },
+        "TGkxzkDKyMeq2T7edKnyjZoFypyzjkkssq": {
+            "cToken": "TD5SdLw5scR6mXgyMK2xKrFJpauDjpKqrW",
+            "liquidationThreshold": 0.75
+        },
+        "TU3kjFuhtEo42tsCBtfYUAZxoqQ4yuSLQ5": {
+            "cToken": "TJQ9rbVe9ei3nNtyGgBL22Fuu2xYjZaLAQ",
+            "liquidationThreshold": 0.75
+        },
+        "TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S": {
+            "cToken": "TPXDpkg9e3eZzxqxAUyke9S4z4pGJBJw9e",
+            "liquidationThreshold": 0.5
+        },
+        "TAFjULxiVgT4qWk6UZwjqwZXTSaGaqnVp4": {
+            "cToken": "TUaUHU9Dy8x5yNi1pKnFYqHWojot61Jfto",
+            "liquidationThreshold": 0.6
+        },
+        "TFczxzPhnThNSqr5by8tvxsdCFRRz6cPNq": {
+            "cToken": "TFpPyDCKvNFgos3g3WVsAqMrdqhB81JXHE",
+            "liquidationThreshold": 0.6
+        },
+        "TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9": {
+            "cToken": "TWQhCXaWz4eHK4Kd1ErSDHjMFPoPc9czts",
+            "liquidationThreshold": 0.6
+        },
+        "TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7": {
+            "cToken": "TRg6MnpsFXc82ymUPgf5qbj59ibxiEDWvv",
+            "liquidationThreshold": 0.5
+        },
+        "TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT": {
+            "cToken": "TL5x9MtSnDy537FXKx53yAaHRRNdg9TkkA",
+            "liquidationThreshold": 0.75
+        },
+        "TEkxiTehnzSmSe2XqrBj4w32RUN966rdz8": {
+            "cToken": "TNSBA6KvSvMoTqQcEgpVK7VhHT3z7wifxy",
+            "liquidationThreshold": 0.75
+        },
+        "TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4": {
+            "cToken": "TSXv71Fy5XdL3Rh2QfBoUu3NAaM4sMif8R",
+            "liquidationThreshold": 0.75
+        },
+        "TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9": {
+            "cToken": "TLeEu311Cbw63BcmMHDgDLu7fnk9fqGcqT",
+            "liquidationThreshold": 0.75
+        },
+        "TRFe3hT5oYhjSZ6f3ji5FJ7YCfrkWnHRvh": {
+            "cToken": "TWBxQMb6RD3qmkXUXpNwVCYbL8SHNreru6",
+            "liquidationThreshold": 0.75
+        },
+        "TKfjV9RNKJJCqPvBtK8L7Knykh7DNWvnYt": {
+            "cToken": "TUY54PVeH6WCcYCd6ZXXoBDsHytN9V5PXt",
+            "liquidationThreshold": 0.6
+        },
+        "TMz2SWatiAtZVVcH2ebpsbVtYwUPT9EdjH": {
+            "cToken": "TLHASseQymmpGQdfAyNjkMXFTJh8nzR2x2",
+            "liquidationThreshold": 0.75
+        },
+        "TKkeiboTkxXKJpbmVFbv4a8ov5rAfRDMf9": {
+            "cToken": "TGBr8uh9jBVHJhhkwSJvQN2ZAKzVkxDmno",
+            "liquidationThreshold": 0.25
+        },
+        "THb4CqiFdwNHsWsQCs4JhzwjMWys4aqCbF": {
+            "cToken": "TR7BUFRQeq1w5jAZf1FKx85SHuX6PfMqsV",
+            "liquidationThreshold": 0.75
+        }
+    }
+}
