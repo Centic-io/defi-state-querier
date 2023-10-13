c4cce4d3777c27e14fe5ee40d3b4a226b4f4a3a3
hoangthanhlamm <hoangthanhlamm@gmail.com>
2023-10-13 16:59:13 +0700

    feat: radiant v2 total suply, borrow, apy

--- example/protocol_apy_information.py
+++ example/protocol_apy_information.py
@@ -3,13 +3,14 @@ import json
 from defi_services.jobs.processors.state_processor import StateProcessor
 
 job = StateProcessor(
-    provider_uri="https://rpc.ankr.com/bsc",
-    chain_id="0x38"
+    # provider_uri="https://rpc.ankr.com/arbitrum",
+    provider_uri="https://nd-800-603-872.p2pify.com/e874cc3efd9e36b8b05bd16a1ab2bf2c",
+    chain_id="0xa4b1"
 )
 queries = [
     {
         "query_id": 4,
-        "entity_id": 'valas-finance',
+        "entity_id": 'radiant-v2',
         "query_type": "protocol_apy"
     }
 ]
