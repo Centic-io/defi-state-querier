e11d0b224a30ab2146d2b30cf0fd5fea0f08e183
hoangthanhlamm <hoangthanhlamm@gmail.com>
2023-12-19 09:07:47 +0700

    feat: trava vault ethereum and fantom

--- src/defi_services/services/vault/vault_info/bsc/trava_bsc.py
+++ src/defi_services/services/vault/vault_info/bsc/trava_bsc.py
@@ -4,9 +4,6 @@ TRAVA_VAULT_BSC = {
     "reservesList": {
         "0x17b173d4b80b0b5bb7e0f1e99f5962f2d51799eb": {
             "vaultName": "rtrava",
-            "createdAt": 1635396194,
-            "closedAt": None,
-            "endedAt": None,
             "tokenIn": "0x170772a06affc0d375ce90ef59c8ec04c7ebf5d2",
             "metadata": {
                 "vaultType": "base",
@@ -15,9 +12,6 @@ TRAVA_VAULT_BSC = {
         },
         "0xc5f0137ce42437d8f5ef25da110f7fd7386178ec": {
             "vaultName": "trava",
-            "createdAt": 1628178186,
-            "closedAt": None,
-            "endedAt": None,
             "tokenIn": "0x0391be54e72f7e001f6bbc331777710b4f2999ef",
             "metadata": {
                 "vaultType": "base",
@@ -26,9 +20,6 @@ TRAVA_VAULT_BSC = {
         },
         "0xf04fee30118fdb83c0957c4f6abdfdde977b9aeb": {
             "vaultName": "trava/bnb",
-            "createdAt": 1628178465,
-            "closedAt": None,
-            "endedAt": None,
             "tokenIn": "0x865c77d4ff6383e06c58350a2cfb95cca2c0f056",
             "metadata": {
                 "vaultType": "lp",
@@ -38,9 +29,6 @@ TRAVA_VAULT_BSC = {
         },
         "0xd5cc214621395686b972dde8481a7463a0dab962": {
             "vaultName": "orai",
-            "createdAt": 1626708184,
-            "closedAt": None,
-            "endedAt": None,
             "tokenIn": "0xa325ad6d9c92b55a3fc5ad7e412b1518f96441c0",
             "metadata": {
                 "vaultType": "normal",
---
+++ src/defi_services/services/vault/vault_info/ethereum/trava_eth.py
@@ -0,0 +1,22 @@
+TRAVA_VAULT_ETH = {
+    "name": "Trava Vault",
+    "rewardToken": "0x186d0ba3dfc3386c464eecd96a61fbb1e2da00bf",
+    "reservesList": {
+        "0xbf576c63c30e1ebb637138ee1c67a570ff70223d": {
+            "vaultName": "rtrava",
+            "tokenIn": "0x044ede67afdb0f56d7451bb3aaccaeab3f772fad",
+            "metadata": {
+                "vaultType": "base",
+                "token": "0x044ede67afdb0f56d7451bb3aaccaeab3f772fad"
+            }
+        },
+        "0x84d92a3d61ec158dcf878ff24ebdcf6b2b5ec1fa": {
+            "vaultName": "trava",
+            "tokenIn": "0x186d0ba3dfc3386c464eecd96a61fbb1e2da00bf",
+            "metadata": {
+                "vaultType": "base",
+                "token": "0x186d0ba3dfc3386c464eecd96a61fbb1e2da00bf"
+            }
+        }
+    }
+}
