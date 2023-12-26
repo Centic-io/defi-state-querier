951a098b011fc71c4a4f9c5d7b49ff0c203d7db2
phamvietbang <superpower99x99@gmail.com>
2023-12-26 16:14:23 +0700

    fix quickswap pool info

--- src/defi_services/constants/entities/dex_services.py
+++ src/defi_services/constants/entities/dex_services.py
@@ -4,10 +4,10 @@ from defi_services.services.dex.quickswap_v2_service import QuickSwapV2Services
 from defi_services.services.dex.spookyswap_v2_service import SpookySwapV2Services
 from defi_services.services.dex.sushiswap_v2_service import SushiSwapV2Services
 from defi_services.services.dex.uniswap_v2_service import UniswapV2Services
-from src.defi_services.constants.chain_constant import Chain
-from src.defi_services.constants.entities.dex_constant import Dex
-from src.defi_services.services.dex.pancakeswap_service import PancakeSwapServices
-from src.defi_services.services.dex.sushiswap_service import SushiSwapServices
+from defi_services.constants.chain_constant import Chain
+from defi_services.constants.entities.dex_constant import Dex
+from defi_services.services.dex.pancakeswap_service import PancakeSwapServices
+from defi_services.services.dex.sushiswap_service import SushiSwapServices
 
 
 class DexServices:
