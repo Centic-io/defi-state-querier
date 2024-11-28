9f392e00660ebbb65129142f2da16c152e8e9af7
hoangthanhlamm <hoangthanhlamm@gmail.com>
2024-11-28 16:29:04 +0700

    feat: optimize tcv query

--- src/defi_services/jobs/tcv.py
+++ src/defi_services/jobs/tcv.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from defi_services.constants.entities.dex_constant import Dex
 from defi_services.constants.query_constant import Query
 from defi_services.jobs.processors.state_processor import StateProcessor
@@ -13,98 +15,130 @@ class TCV:
             reserves_info,
             block_number: int = "latest",
     ):
-        queries = [
-            {
-                "query_id": "tcv-vault",
-                "entity_id": "tcv-vault",
-                "query_type": Query.staking_reward
-            },
-        ]
-        res = self.state_processor.run(address, queries, block_number)
-        result = res[0].get("staking_reward")
-        for token, information in reserves_info.items():
-            pool = information.get("pool")
-            lp_token_dict = {
-                pool: reserves_info[token]["poolInfo"]
-            }
-            dex_lp_info = self.get_lp_token_info(token, Dex.uniswap_v3, lp_token_dict)
-            token_nfts = self.get_user_nft(token, Dex.uniswap_v3)
-            token_info = self.get_user_info(token, Dex.uniswap_v3, token_nfts)
-            token_balance = self.get_user_token_balance(token, Dex.uniswap_v3, token_info, dex_lp_info)
-            tvl = {}
-            for balance_info in token_balance:
-                for token_id, info in balance_info['dex_user_token_balance'].items():
-                    if info['token0'] not in tvl:
-                        tvl[info['token0']] = 0
-                    if info['token1'] not in tvl:
-                        tvl[info['token1']] = 0
-                    tvl[info['token0']] += info['token0_amount']
-                    tvl[info['token1']] += info['token1_amount']
-            result[token].update({"tvl": tvl})
-        return result
+        lp_token_dict = {information["pool"]: information["poolInfo"] for information in reserves_info.values()}
+        tcv_liquidity_user, dex_lp_info = self.get_tcv_liquidity_user_and_lp_token_info(
+            wallet=address,
+            dex_protocol=Dex.uniswap_v3, lp_token_list=lp_token_dict,
+            block_number=block_number
+        )
 
-    def get_user_nft(self, wallet, dex_protocol):
+        tcv_nfts = {}
+        for vault_address, liquidity_info in tcv_liquidity_user.items():
+            # For optimize
+            liquidity_user = liquidity_info.get('liquidity_user')
+            if not liquidity_user:
+                liquidity_info.update({'tvl': {}})
+                continue
+
+            vault_nft = self.get_user_nft(vault_address, Dex.uniswap_v3, block_number=block_number)
+            tcv_nfts.update(vault_nft)
+
+        nfts_info = self.get_nfts_info(address, Dex.uniswap_v3, tcv_nfts, block_number=block_number)
+        user_balance = self.get_user_token_balance(address, Dex.uniswap_v3, nfts_info, dex_lp_info, block_number=block_number)
+
+        for token_id, info in user_balance.items():
+            pool_address = info['pool_address']
+            addresses = [vault_address_ for vault_address_, info in reserves_info.items() if info['pool'] == pool_address]
+            vault_address = addresses[0] if addresses else None
+
+            if (not vault_address) or (vault_address not in tcv_liquidity_user):
+                continue
+
+            if tcv_liquidity_user[vault_address].get('tvl') is None:
+                tcv_liquidity_user[vault_address]['tvl'] = defaultdict(lambda: 0)
+
+            tcv_liquidity_user[vault_address]['tvl'][info['token0']] += info['token0_amount']
+            tcv_liquidity_user[vault_address]['tvl'][info['token1']] += info['token1_amount']
+
+        return tcv_liquidity_user
+
+    def get_user_nft(self, wallet, dex_protocol, block_number: int = 'latest'):
         queries = [
             {
-                'query_id': f'{dex_protocol}_usernft',
+                'query_id': f'{dex_protocol}_{Query.dex_user_nft}',
                 "entity_id": dex_protocol,
                 'query_type': Query.dex_user_nft
             }
         ]
 
-        res = self.state_processor.run(wallet, queries, batch_size=100, max_workers=8, ignore_error=True)
-        return res
+        res = self.state_processor.run(
+            wallet, queries, block_number=block_number,
+            batch_size=100, max_workers=8, ignore_error=True
+        )
+        user_nfts = res[0][Query.dex_user_nft]
+        return user_nfts
 
-    def get_user_info(self, wallet, dex_protocol, user_nfts):
+    def get_nfts_info(self, wallet, dex_protocol, user_nfts, block_number: int = 'latest'):
         queries = [
             {
-                'query_id': f'{dex_protocol}_userinfo',
+                'query_id': f'{dex_protocol}_{Query.dex_user_info}',
                 "entity_id": dex_protocol,
                 'query_type': Query.dex_user_info,
                 'supplied_data': {
-                    'user_data': user_nfts[0][Query.dex_user_nft],
+                    'user_data': user_nfts,
                 }
             }
         ]
 
-        res = self.state_processor.run(wallet, queries, batch_size=100, max_workers=8, ignore_error=True)
-        return res
+        res = self.state_processor.run(
+            wallet, queries, block_number=block_number,
+            batch_size=100, max_workers=8, ignore_error=True
+        )
+        nfts_info = res[0][Query.dex_user_info]
+        return nfts_info
 
-    def get_user_token_balance(self, wallet, dex_protocol, user_info, dex_lp_info):
+    def get_user_token_balance(self, wallet, dex_protocol, nfts_info, lp_token_info, block_number: int = 'latest'):
         queries = [
             {
-                'query_id': f'{dex_protocol}_usertokenbalance',
+                'query_id': f'{dex_protocol}_{Query.dex_user_token_balance}',
                 "entity_id": dex_protocol,
                 'query_type': Query.dex_user_token_balance,
                 'supplied_data': {
-                    'user_data': user_info[0][Query.dex_user_info],
-                    'lp_token_info': dex_lp_info[0][Query.lp_token_info]
-
+                    'user_data': nfts_info,
+                    'lp_token_info': lp_token_info
                 }
             }
         ]
 
-        res = self.state_processor.run(wallet, queries, batch_size=100, max_workers=8, ignore_error=True)
-        return res
+        res = self.state_processor.run(
+            wallet, queries, block_number=block_number,
+            batch_size=100, max_workers=8, ignore_error=True
+        )
+        user_balance = res[0][Query.dex_user_token_balance]
+        return user_balance
 
-    def get_lp_token_info(self, wallet, dex_protocol, lp_token_list):
+    def get_tcv_liquidity_user_and_lp_token_info(self, wallet, dex_protocol, lp_token_list, block_number: int = 'latest'):
         queries = [
             {
-                'query_id': f'{dex_protocol}_lptokeninfo',
+                "query_id": f"tcv-vault_{Query.staking_reward}",
+                "entity_id": "tcv-vault",
+                "query_type": Query.staking_reward
+            },
+            {
+                'query_id': f'{dex_protocol}_{Query.lp_token_info}',
                 "entity_id": dex_protocol,
                 'query_type': Query.lp_token_info,
                 'supplied_data': {
-                    'lp_token_info': lp_token_list}
+                    'lp_token_info': lp_token_list
+                }
             },
-            {
-                'query_id': f'{dex_protocol}_lptokenbalance',
-                "entity_id": dex_protocol,
-                'query_type': Query.token_pair_balance,
-                'supplied_data': {
-                    'lp_token_info': lp_token_list}
-            }
+            # {
+            #     'query_id': f'{dex_protocol}_{Query.token_pair_balance}',
+            #     "entity_id": dex_protocol,
+            #     'query_type': Query.token_pair_balance,
+            #     'supplied_data': {
+            #         'lp_token_info': lp_token_list
+            #     }
+            # }
         ]
-        res = self.state_processor.run(wallet, queries, batch_size=100, max_workers=8, ignore_error=True)
-        return res
+        res = self.state_processor.run(
+            address=wallet, queries=queries,
+            block_number=block_number,
+            batch_size=100, max_workers=8,
+            ignore_error=True
+        )
+        data = {r['query_id']: r for r in res}
 
-
+        tcv_liquidity_user = data.get(f'tcv-vault_{Query.staking_reward}', {}).get(Query.staking_reward)
+        lp_token_info = data.get(f'{dex_protocol}_{Query.lp_token_info}', {}).get(Query.lp_token_info)
+        return tcv_liquidity_user, lp_token_info
--- src/defi_services/services/vault/vault_info/arbitrum/tcv_arb.py
+++ src/defi_services/services/vault/vault_info/arbitrum/tcv_arb.py
@@ -7,7 +7,7 @@ TCV_VAULT_ARBITRUM = {
             "vaultName": "eth/usdc",
             "tokenIn": "0xe467db55710cf35e2dc0402104e23221f0e12e66",
             "pool": '0xc6962004f452be9203591991d15f6b388e09e8d0',
-            "poolInfo":{
+            "poolInfo": {
                 "token0": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
                 "token1": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
                 "fee": 500
@@ -17,7 +17,7 @@ TCV_VAULT_ARBITRUM = {
             "vaultName": "eth/usdt",
             "tokenIn": "0x9403be93fddedf88a3ed7d11bfb643b13e5cbc27",
             "pool": '0x641c00a822e8b671738d32a431a4fb6074e5c79d',
-            "poolInfo":{
+            "poolInfo": {
                 "token0": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
                 "token1": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9",
                 "fee": 500
@@ -28,7 +28,7 @@ TCV_VAULT_ARBITRUM = {
             "vaultName": "eth/arb",
             "tokenIn": "0xae83866e6b48e29b225a10bc236f2665cc4f081e",
             "pool": '0xc6f780497a95e246eb9449f5e4770916dcd6396a',
-            "poolInfo":{
+            "poolInfo": {
                 "token0": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
                 "token1": "0x912ce59144191c1204e64559fe8253a0e49e6548",
                 "fee": 500
@@ -38,7 +38,7 @@ TCV_VAULT_ARBITRUM = {
             "vaultName": "eth/usdc(bridgeusdc)",
             "tokenIn": "0xaae3866f0233aec20580c21a3c10791afd38c8c1",
             "pool": '0xc31e54c7a869b9fcbecc14363cf510d1c41fa443',
-            "poolInfo":{
+            "poolInfo": {
                 "token0": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
                 "token1": "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8",
                 "fee": 500
@@ -48,7 +48,7 @@ TCV_VAULT_ARBITRUM = {
             "vaultName": "eth/usdc",
             "tokenIn": "0xa02d1ecbe8eefaf7d64871ee0ae6404318df9702",
             "pool": '0xc473e2aee3441bf9240be85eb122abb059a3b57c',
-            "poolInfo":{
+            "poolInfo": {
                 "token0": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1",
                 "token1": "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
                 "fee": 3000
