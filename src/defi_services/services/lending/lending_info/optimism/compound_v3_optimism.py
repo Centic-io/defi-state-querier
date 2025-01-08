28d7bfaf6caab25f2b66dd7fae63b92bfed68cc5
hoangthanhlamm <hoangthanhlamm@gmail.com>
2025-01-08 15:18:54 +0700

    feat: compound v3 on Base

---
+++ src/defi_services/services/lending/lending_info/optimism/compound_v3_optimism.py
@@ -0,0 +1,116 @@
+COMPOUND_V3_OPTIMISM = {
+    "name": "Compound V3 Lending Pool",
+    "rewardToken": "0x7e7d4467112689329f7e06571ed0e8cbad4910ee",
+    "rewardAddress": "0x443ea0340cb75a160f31a440722dec7b5bc3c2e9",
+    "factoryAddress": "0xfa454de61b317b6535a0c462267208e8fdb89f45",
+    "poolToken": "0x7e7d4467112689329f7e06571ed0e8cbad4910ee",
+    "type": "LENDING_POOL",
+    "forked": "compound-v3",
+    "reservesList": {
+        "0x4200000000000000000000000000000000000006": {
+            "comet": "0xe36a30d249f7761327fd973001a32010b521b6fd",
+            "assets": {
+                "0x1f32b1c2345538c0c6f582fcb022739c4a194ebb": {
+                    "priceFeed": "0x295d9f81298fc44b4f329cbef57e67266091cc86",
+                    "loanToValue": 0.88,
+                    "liquidationThreshold": 0.93
+                },
+                "0x9bcef72be871e61ed4fbbc7630889bee758eb81d": {
+                    "priceFeed": "0x5d409e56d886231adaf00c8775665ad0f9897b56",
+                    "loanToValue": 0.9,
+                    "liquidationThreshold": 0.93
+                },
+                "0x68f180fcce6836688e9084f035309e29bf0a2095": {
+                    "priceFeed": "0x4ed39cf78ffa4428de6bcedb8d0f5ff84699e13d",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58": {
+                    "priceFeed": "0xddc326838f2b5e5625306c3cf33318666f3cf002",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x0b2c639c533813f4aa9d7837caf62653d097ff85": {
+                    "priceFeed": "0x403f2083b6e220147f8a8832f0b284b4ed5777d1",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x2416092f143378750bb29b79ed961ab195cceea5": {
+                    "priceFeed": "0x69dd37a0ee3d15776a54e34a7297b059e75c94ab",
+                    "loanToValue": 0.88,
+                    "liquidationThreshold": 0.91
+                },
+                "0x5a7facb970d094b6c7ff1df0ea68d99e6e73cbff": {
+                    "priceFeed": "0x5a20166ed55518520b1f68cab1cf127473123814",
+                    "loanToValue": 0.9,
+                    "liquidationThreshold": 0.93
+                },
+                "0x87eee96d50fb761ad85b1c982d28a042169d61b1": {
+                    "priceFeed": "0xee7b99dc798eae1957713bbbede98073098b0e68",
+                    "loanToValue": 0.88,
+                    "liquidationThreshold": 0.91
+                }
+            }
+        },
+        "0x0b2c639c533813f4aa9d7837caf62653d097ff85": {
+            "comet": "0x2e44e174f7d53f0212823acc11c01a11d58c5bcb",
+            "assets": {
+                "0x4200000000000000000000000000000000000042": {
+                    "priceFeed": "0x0d276fc14719f9292d5c1ea2198673d1f4269246",
+                    "loanToValue": 0.65,
+                    "liquidationThreshold": 0.7
+                },
+                "0x4200000000000000000000000000000000000006": {
+                    "priceFeed": "0x13e3ee699d1909e989722e753853ae30b17e08c5",
+                    "loanToValue": 0.83,
+                    "liquidationThreshold": 0.9
+                },
+                "0x68f180fcce6836688e9084f035309e29bf0a2095": {
+                    "priceFeed": "0x718a5788b89454aae3a028ae9c111a29be6c2a6f",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x1f32b1c2345538c0c6f582fcb022739c4a194ebb": {
+                    "priceFeed": "0x4f90c34def3516ca5bd0a8276e01516fb09fb2ab",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x57f5e098cad7a3d1eed53991d4d66c45c9af7812": {
+                    "priceFeed": "0x7e86318cc4bc539043f204b39ce0ebed9f0050dc",
+                    "loanToValue": 0.88,
+                    "liquidationThreshold": 0.9
+                }
+            }
+        },
+        "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58": {
+            "comet": "0x995e394b8b2437ac8ce61ee0bc610d617962b214",
+            "assets": {
+                "0x4200000000000000000000000000000000000042": {
+                    "priceFeed": "0x0d276fc14719f9292d5c1ea2198673d1f4269246",
+                    "loanToValue": 0.65,
+                    "liquidationThreshold": 0.7
+                },
+                "0x4200000000000000000000000000000000000006": {
+                    "priceFeed": "0x13e3ee699d1909e989722e753853ae30b17e08c5",
+                    "loanToValue": 0.83,
+                    "liquidationThreshold": 0.9
+                },
+                "0x68f180fcce6836688e9084f035309e29bf0a2095": {
+                    "priceFeed": "0x718a5788b89454aae3a028ae9c111a29be6c2a6f",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x1f32b1c2345538c0c6f582fcb022739c4a194ebb": {
+                    "priceFeed": "0x6846fc014a72198ee287350ddb6b0180586594e5",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0x57f5e098cad7a3d1eed53991d4d66c45c9af7812": {
+                    "priceFeed": "0x66228d797eb83ecf3465297751f6b1d4d42b7627",
+                    "loanToValue": 0.88,
+                    "liquidationThreshold": 0.9
+                }
+            }
+        }
+    }
+}
--- src/defi_services/services/lending/lending_info/polygon/compound_v3_polygon.py
+++ src/defi_services/services/lending/lending_info/polygon/compound_v3_polygon.py
@@ -12,18 +12,18 @@ COMPOUND_V3_POLYGON = {
             "assets": {
                 "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": {
                     "priceFeed": "0xf9680d99d6c9589e2a93a78a04a279e509205945",
-                    "loanToValue": 0.775,
-                    "liquidationThreshold": 0.825
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
                 },
                 "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6": {
                     "priceFeed": "0xde31f8bfbd8c84b5360cfacca3539b938dd78ae6",
-                    "loanToValue": 0.7,
-                    "liquidationThreshold": 0.75
+                    "loanToValue": 0.75,
+                    "liquidationThreshold": 0.85
                 },
                 "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270": {
                     "priceFeed": "0xab594600376ec9fd91f8e885dadf0ce036862de0",
                     "loanToValue": 0.65,
-                    "liquidationThreshold": 0.7
+                    "liquidationThreshold": 0.8
                 },
                 "0xfa68fb4628dff1028cfec22b4162fccd0d45efb6": {
                     "priceFeed": "0x5d37e4b374e6907de8fc7fb33ee3b0af403c7403",
@@ -34,6 +34,36 @@ COMPOUND_V3_POLYGON = {
                     "priceFeed": "0x97371df4492605486e23da797fa68e55fc38a13f",
                     "loanToValue": 0.6,
                     "liquidationThreshold": 0.65
+                }
+            }
+        },
+        "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": {
+            "comet": "0xaeb318360f27748acb200ce616e389a6c9409a07",
+            "assets": {
+                "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270": {
+                    "priceFeed": "0xab594600376ec9fd91f8e885dadf0ce036862de0",
+                    "loanToValue": 0.65,
+                    "liquidationThreshold": 0.8
+                },
+                "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": {
+                    "priceFeed": "0xf9680d99d6c9589e2a93a78a04a279e509205945",
+                    "loanToValue": 0.8,
+                    "liquidationThreshold": 0.85
+                },
+                "0xfa68fb4628dff1028cfec22b4162fccd0d45efb6": {
+                    "priceFeed": "0x5d37e4b374e6907de8fc7fb33ee3b0af403c7403",
+                    "loanToValue": 0.6,
+                    "liquidationThreshold": 0.7
+                },
+                "0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4": {
+                    "priceFeed": "0x97371df4492605486e23da797fa68e55fc38a13f",
+                    "loanToValue": 0.6,
+                    "liquidationThreshold": 0.7
+                },
+                "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6": {
+                    "priceFeed": "0xde31f8bfbd8c84b5360cfacca3539b938dd78ae6",
+                    "loanToValue": 0.75,
+                    "liquidationThreshold": 0.85
                 }
             }
         }
