cf349b2a55c1ce6f41f37d118973515fe058b096
bang <phamvietbang2965@gmail.com>
2025-01-08 14:42:22 +0700

    add aave v3 in bsc

--- src/defi_services/services/lending/lending_info/bsc/aave_v3_bsc.py
+++ src/defi_services/services/lending/lending_info/bsc/aave_v3_bsc.py
@@ -2,7 +2,7 @@ AAVE_V3_BSC = {
     "address": "0x6807dc923806fe8fd134338eabca509979a7e0cb",
     "name": "AAVE V3 Lending Pool",
     "stakedIncentiveAddress": '0xc206c2764a9dbf27d599613b8f9a63acd1160ab4',
-    "rewardTokensList": [],
+    "rewardTokensList": ["0xc5f0f7b66764F6ec8C8Dff7BA683102295E16409"],
     "oracleAddress": "0x39bc1bfda2130d6bb6dbefd366939b4c7aa7c697",
     "type": "LENDING_POOL",
     "poolToken": "0xfb6115445bff7b52feb98650c87f44907e58f802",
