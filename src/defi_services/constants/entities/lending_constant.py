fe495209f5d17a4ea445868723b62949ccb75a2a
bang <phamvietbang2965@gmail.com>
2025-01-14 11:23:25 +0700

    push version 22

--- src/defi_services/constants/entities/lending_constant.py
+++ src/defi_services/constants/entities/lending_constant.py
@@ -29,5 +29,5 @@ class Lending:
     all = [strike, aave_v2, aave_v3, radiant_v2, compound, flux, onyx, granary,
            iron_bank, trava, valas, geist, cream, venus, liqee, strike, uwu, wepiggy,
            morpho_compound, morpho_aave_v2, morpho_aave_v3, spark, ape_swap, silo, compound_v3,
-           justlend
+           justlend, zerolend
            ]
