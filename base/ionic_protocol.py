f826d7fc7c5d965359f5085c6e64d54325715561
hoangthanhlamm <hoangthanhlamm@gmail.com>
2025-01-15 22:57:00 +0700

    fix: remove base in root folder

--- base/ionic_protocol.py
+++
@@ -1,9 +0,0 @@
-IONIC_PROTOCOL = {
-    "name": "Ionic Protocol Lending Pool",
-    "rewardToken": "",
-    "rewardAddress": "",
-    "factoryAddress": "",
-    "poolToken": "0x3eE5e23eEE121094f1cFc0Ccc79d6C809Ebd22e5",
-    "type": "LENDING_POOL",
-    "forked": "compound-v2",
-}
